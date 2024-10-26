88f5da24d270939f89debfc1ce482ec4b0f3da63
KREnZE <75460347+KREnZE1@users.noreply.github.com>
2024-10-26 14:50:00 +0200

    Started work on first few screens

---
+++ app/src/main/java/com/krenze/notable/activities/ParentActivity.java
@@ -0,0 +1,19 @@
+package com.krenze.notable.activities;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class ParentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setTitle("Notable");
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    public void onBackPressed() {}
+}
